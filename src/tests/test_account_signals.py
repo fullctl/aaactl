176596d5ff7f89aef906b07f94a82825bb17cca5
Stefan Pratter <stefan@20c.com>
2020-09-30 14:08:50 +0000

    fix tests

--- src/tests/test_account_signals.py
+++ src/tests/test_account_signals.py
@@ -2,12 +2,6 @@ def test_generate_api_key(db, account_objects):
     assert account_objects.user.key_set.count() == 1
     assert account_objects.user.key_set.first().managed == False
 
-
-def test_set_initial_permissions(db, account_objects):
-    perms = account_objects.perms
-    assert perms.check("user.{}".format(account_objects.user.id), "crud")
-
-
 def test_create_personal_org(db, account_objects):
     assert account_objects.user.personal_org
 

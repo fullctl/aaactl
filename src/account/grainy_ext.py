3cc529f6547f34b61ee51e3196fee1f7e3ad96f9
Stefan Pratter <stefan@20c.com>
2021-05-14 11:00:29 +0000

    fix api key passthrough for remote grainy retrieve

--- src/account/grainy_ext.py
+++ src/account/grainy_ext.py
@@ -7,11 +7,15 @@ class APIKeyAuthenticator(Authenticator):
     def authenticate(self, request):
 
         permission_holder, _ = APIKeyAuthentication.authenticate(self, request)
+        User = get_user_model()
 
         # personal api key, grab permissions for owning user
 
         if isinstance(permission_holder, APIKey):
             request.user = permission_holder.user
+
+        if isinstance(permission_holder, User):
+            request.user = permission_holder
 
         # internal api keys can be used to grab permission definitions
         # for a specific user

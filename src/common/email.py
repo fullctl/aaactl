1b3984a7c4098666bfeb477fa71b5a74c81589ae
Stefan Pratter <stefan@20c.com>
2021-08-31 10:03:52 +0000

    use logging

--- src/common/email.py
+++ src/common/email.py
@@ -2,6 +2,9 @@ from django.conf import settings
 from django.core.mail import send_mail
 
 
+import logging
+logger = logging.getLogger("django")
+
 def email_noreply(address, subject, message, **kwargs):
     email(settings.DEFAULT_FROM_EMAIL, [address], subject, message, **kwargs)
 
@@ -11,12 +14,12 @@ def email(sender, addresses, subject, message, **kwargs):
     for address in addresses:
 
         if settings.DEBUG_EMAIL:
-            print("FROM", sender)
-            print("TO", address)
-            print("SUBJ", subject)
-            print("--------------")
-            print(message)
-            print("--------------")
+            logger.info("FROM: "+ sender)
+            logger.info("TO: "+ address)
+            logger.info("SUBJ: "+ subject)
+            logger.info("--------------")
+            logger.info(message)
+            logger.info("--------------")
             continue
 
         send_mail(f"{subject}", f"{message}", sender, [address], **kwargs)

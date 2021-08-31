ec3da678842310dd4de47f667ee5e467ff8cc989
Stefan Pratter <stefan@20c.com>
2021-08-31 09:35:58 +0000

    service bridge additions for aaactl sync endpoint defaults

--- src/applications/service_bridge.py
+++ src/applications/service_bridge.py
@@ -4,7 +4,7 @@ from account.models import InternalAPIKey
 
 
 class Bridge(client.Bridge):
-    def __init__(self, service, org):
+    def __init__(self, service, org, user=None):
 
         api_key = InternalAPIKey.objects.first()
 
@@ -13,20 +13,49 @@ class Bridge(client.Bridge):
 
         super().__init__(service.api_host, api_key.key, org.slug)
 
+        self.org_id = org.id
+
+        self.user = user
+        if user:
+            self.user_id = user.id
+        else:
+            self.user_id = 0
+
         self.service = service
 
     def usage(self, product_name):
-        endpoint = self._endpoint("usage")
-        if not endpoint:
-            raise KeyError(f"No usage endpoint specified for {self.service.name}")
-
-        path = endpoint.path.format(org=self.org)
-
+        # this should stay {org} and not {org_id} so it uses the org slug
+        path = self._endpoint("usage", default="usage/{org}")
         data = self.get(path)
         for row in data:
             if row["name"] == product_name:
                 return row["units"]
         return None
 
-    def _endpoint(self, name):
-        return self.service.api_endpoint_set.filter(name=name).first()
+    def sync_user(self, user_id):
+        path = self._endpoint("sync_user", default="aaactl-sync/user/{user}")
+        self.put(path)
+
+    def sync_org(self):
+        path = self._endpoint("sync_org", default="aaactl-sync/org/{org_id}")
+        self.put(path)
+
+    def add_orguser(self, user_id):
+        path = self._endpoint("sync_orguser", default="aaactl-sync/orguser/{org_id}")
+        self.post(path)
+
+    def del_orguser(self, user_id):
+        path = self._endpoint("sync_orguser", default="aaactl-sync/orguser/{org_id}")
+        self.post(path)
+
+    def _endpoint(self, name, default=None):
+        endpoint = self.service.api_endpoint_set.filter(name=name).first()
+        if not endpoint:
+            if default:
+                path = default
+            else:
+                raise KeyError(f"No {name} endpoint specified for {self.service.name}")
+        else:
+            path = endpoint.path
+
+        return path.format(org=self.org, org_id=self.org_id, user=self.user_id)

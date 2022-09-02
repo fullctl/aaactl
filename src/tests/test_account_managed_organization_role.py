3e3af52d379279b3fea3d3d3b30dc6055a4a02d2
Jason Carvalho <jason@20c.com>
2022-09-02 04:36:24 +0000

    add tests

---
+++ src/tests/test_account_managed_organization_role.py
@@ -0,0 +1,45 @@
+import pytest
+from django_grainy.util import Permissions
+from grainy.const import PERM_CRUD
+
+from account.models import (
+    ManagedPermission,
+    ManagedPermissionRoleAutoGrant,
+    OrganizationRole,
+    Role,
+)
+
+
+@pytest.mark.django_db
+def test_organization_role_add_and_delete(account_objects):
+
+    test_role = Role.objects.create(
+        name="test",
+        description="",
+        level=15,
+        auto_set_on_creator=True,
+        auto_set_on_member=False,
+    )
+
+    mperm = ManagedPermission.objects.create(
+        namespace="test.mperm",
+        status="ok",
+        group="test",
+        description="test",
+        managable=True,
+    )
+
+    ManagedPermissionRoleAutoGrant.objects.create(
+        managed_permission=mperm, role=test_role, permissions=PERM_CRUD
+    )
+
+    org_role = OrganizationRole.objects.create(
+        org=account_objects.org, user=account_objects.user, role=test_role
+    )
+    ManagedPermission.apply_roles_all()
+
+    assert Permissions(account_objects.user).check("test.mperm", "r")
+
+    org_role.delete()
+
+    assert not Permissions(account_objects.user).check("test.mperm", "r")

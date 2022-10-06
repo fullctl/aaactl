7dc358a7fd622c0fb7fe7c9bb4e17512a3c1359d
Jason Carvalho <jason@20c.com>
2022-10-06 03:49:16 +0000

    add way to pass in args to docker-compose command

--- Ctl/dev/run.sh
+++ Ctl/dev/run.sh
@@ -3,4 +3,16 @@
 
 COMPOSE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-docker-compose -f $COMPOSE_DIR/docker-compose.yml run account_django $@
+# count number of arguments in the format `-arg value`
+n=1
+while [ $n -le $# ]
+do
+    if [ ${!n:0:1} == "-" ]; then
+        n=$((n + 2))
+    else
+        break
+    fi
+done
+
+# put args in format `-arg value` before service args
+docker-compose -f $COMPOSE_DIR/docker-compose.yml run ${@:1:$n-1} account_django ${@:$n:$#}

8a7f9ed442f0978f1c93e300a6ffadcba91d70fe
Jason Carvalho <jason@20c.com>
2023-02-08 19:32:38 +0000

    linting

--- src/whitelabel/apps.py
+++ src/whitelabel/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class WhitelabelConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'whitelabel'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "whitelabel"

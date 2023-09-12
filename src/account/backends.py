7affe5e16466ad25366122a50ea0a406cf938043
Jason Mishike <98209208+jason20c@users.noreply.github.com>
2023-09-12 20:12:22 +0530

    Add option to login via email (#130)
    * add option to login via email
    
    * refactor EmailOrUsernameModelBackend to use filter
    
    * fix not handling None user in EmailOrUsernameModelBackend

---
+++ src/account/backends.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import User
+from django.db.models import Q
+
+
+class EmailOrUsernameModelBackend:
+    def authenticate(self, request, username=None, password=None):
+        user = User.objects.filter(Q(email=username) | Q(username=username)).first()
+        if user is None:
+            return None
+
+        return user if user.check_password(password) else None
+
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None

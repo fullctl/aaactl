887b88d883b8eb364d4e142139406f5da7ab9b0d
Stefan Pratter <stefan@20c.com>
2023-10-04 12:38:05 +0000

    vscode mishap

--- src/tests/test_billing_views.py
+++ src/tests/test_billing_views.py
@@ -3,73 +3,71 @@ from django.urls import reverse
 
 
 @pytest.mark.django_db
-def test_order_history_details(client_anon, billing_objects_w_pay, charge_objects):
+def test_order_history_details(client_anon, billing_objects, charge_objects):
     order_item_id = charge_objects["order_history"].order_id
 
     # Test anon
     anon_response = client_anon.get(
         reverse("billing:order-history-details", args=[order_item_id])
-        + f"?org={billing_objects_w_pay.org.slug}"
+        + f"?org={billing_objects.org.slug}"
     )
     assert anon_response.status_code == 302
 
     # Test non-existing order history item
-    response = billing_objects_w_pay.client.get(
+    response = billing_objects.client.get(
         reverse("billing:order-history-details", args=[100])
-        + f"?org={billing_objects_w_pay.org.slug}"
+        + f"?org={billing_objects.org.slug}"
     )
     assert response.status_code == 404
 
-    response = billing_objects_w_pay.client.get(
+    response = billing_objects.client.get(
         reverse("billing:order-history-details", args=[order_item_id])
-        + f"?org={billing_objects_w_pay.org.slug}"
+        + f"?org={billing_objects.org.slug}"
     )
     assert response.status_code == 200
 
 
 @pytest.mark.django_db
-def test_billing_contacts(client_anon, billing_objects_w_pay):
+def test_billing_contacts(client_anon, billing_objects):
     anon_response = client_anon.get(reverse("billing:billing-contacts"))
     assert anon_response.status_code == 302
 
-    response = billing_objects_w_pay.client.get(
-        reverse("billing:billing-contacts") + f"?org={billing_objects_w_pay.org.slug}"
+    response = billing_objects.client.get(
+        reverse("billing:billing-contacts") + f"?org={billing_objects.org.slug}"
     )
     assert response.status_code == 200
-    assert billing_objects_w_pay.billing_contact.name in str(response.content)
+    assert billing_objects.billing_contact.name in str(response.content)
 
 
 @pytest.mark.django_db
-def test_services(client_anon, billing_objects_w_pay):
+def test_services(client_anon, billing_objects):
     anon_response = client_anon.get(reverse("billing:services"))
     assert anon_response.status_code == 302
 
-    response = billing_objects_w_pay.client.get(
-        reverse("billing:services") + f"?org={billing_objects_w_pay.org.slug}"
+    response = billing_objects.client.get(
+        reverse("billing:services") + f"?org={billing_objects.org.slug}"
     )
     assert response.status_code == 200
 
 
 @pytest.mark.django_db
-def test_setup(client_anon, billing_objects_w_pay):
+def test_setup(client_anon, billing_objects):
     anon_response = client_anon.get(reverse("billing:setup"))
     assert anon_response.status_code == 302
 
-    response = billing_objects_w_pay.client.get(
-        reverse("billing:setup") + f"?org={billing_objects_w_pay.org.slug}"
+    response = billing_objects.client.get(
+        reverse("billing:setup") + f"?org={billing_objects.org.slug}"
     )
     assert response.status_code == 200
 
 
 @pytest.mark.django_db
-def test_setup_billing_contact_prepop(billing_objects_w_pay):
-    response = billing_objects_w_pay.client.get(
+def test_setup_billing_contact_prepop(billing_objects):
+    response = billing_objects.client.get(
         reverse("billing:setup")
-        + f"?billing_contact={billing_objects_w_pay.billing_contact.id}"
+        + f"?billing_contact={billing_objects.billing_contact.id}"
         + "&"
-        + f"org={billing_objects_w_pay.org.slug}"
+        + f"org={billing_objects.org.slug}"
     )
     assert response.status_code == 200
-    assert billing_objects_w_pay.billing_contact.name in response.content.decode(
-        "utf-8"
-    )
+    assert billing_objects.billing_contact.name in response.content.decode("utf-8")

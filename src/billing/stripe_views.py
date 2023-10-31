6300adcac5fa1ef1992b95f550b56b78b52e4d4c
Stefan Pratter <stefan@20c.com>
2023-10-31 19:12:14 +0200

    Fix stripe payment methods (#135)
    * explicitly set payment method option settings
    
    * linting

--- src/billing/stripe_views.py
+++ src/billing/stripe_views.py
@@ -17,7 +17,9 @@ stripe.api_key = settings.STRIPE_SECRET_KEY
 @login_required
 def create_setup_intent(request):
     """Create a SetupIntent and return its client secret."""
-    setup_intent = stripe.SetupIntent.create()
+    setup_intent = stripe.SetupIntent.create(
+        automatic_payment_methods={"enabled": True}, payment_method_options=None
+    )
 
     data = {
         "client_secret": setup_intent.client_secret,
@@ -33,7 +35,6 @@ def create_setup_intent(request):
 def check_setup_intent(request, id):
     """Check if the SetupIntent is successful."""
     setup_intent = stripe.SetupIntent.retrieve(id)
-
     data = {
         "client_secret": setup_intent.client_secret,
         "status": setup_intent.status,

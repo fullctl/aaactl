67048f37a9756b4b65fce9bfc2b1d354c0398bf3
Genza999 <cartpix@gmail.com>
2024-04-12 22:35:29 +0300

    fix: add dark and light logos

--- src/django_aaactl/rest/serializers/service_bridge.py
+++ src/django_aaactl/rest/serializers/service_bridge.py
@@ -237,4 +237,4 @@ class OauthAccessToken(ModelSerializer):
 class OrganizationWhiteLabelingSerializer(ModelSerializer):
     class Meta:
         model = whitelabel_models.OrganizationWhiteLabeling
-        fields = ['org', 'html_header', 'html_footer', 'css', 'logo_url']
+        fields = ['org', 'html_header', 'html_footer', 'css', 'light_logo_url', 'dark_logo_url']
